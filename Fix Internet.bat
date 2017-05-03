d232351467c9e8f383aad800efc1f6b336b8ee83
JustinTrautman <justin.trautman@gmail.com>
2017-05-03 10:12:03 -0700

    Update Fix Internet.bat

--- Fix Internet.bat
+++ Fix Internet.bat
@@ -2,13 +2,13 @@ echo Fixing internet!
 # Flush DNS Cache
 ipconfig /flushdns
 echo DNS cache successfully cleaned!
-timeout /t 2
+timeout /t 4
 ipconfig /release
 echo IP successfully released!
-timeout /t 2
+timeout /t 4
 ipconfig /renew
 echo IP successfully renewed!
-timeout /t 2
+timeout /t 4
 echo Interent should be fixed! If problems persist, please call the tech.
 echo Press enter to exit.
 pause
